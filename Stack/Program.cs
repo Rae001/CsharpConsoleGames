5bcb36398a32bfea389ccc9841e24042d6f95cae
Rae001 <tmdfo50@gmail.com>
2023-06-26 20:17:03 +0900

    2023/06/26

---
+++ Stack/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace Stack
+{
+    class Program
+    {
+
+        static int width = 30;
+        static int height = 20;
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void Board()
+        {
+            
+        }
+    }
+}
